405e4acb2223a5b1207ff25858bdb92376d4725f
Ben Williams <bcbwilla@gmail.com>
2013-08-05 13:54:25 -0400

    removed old files

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from MineTSim import MineTSim
 from data.brackets import *
 
-m = MineTSim(matches,method="genetic",cycles=300, size=100)
+m = MineTSim(matches,method="monte",cycles=10000)
 m.add_teams('data/rotlw_stats.csv', append_s='rotlw')
 m.add_teams('data/aotc_stats.csv', append_s='aotc')
 m.run()
